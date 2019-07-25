6da960e923ebe53a764b0943a10d97701bcd5fb0
kimchaeyoung <kimchaeyoung@github.com>
2019-07-25 12:10:40 +0900

    fixed files

--- server/api/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    name = 'api'
-    verbose_name = 'Api Configuration for Stone'
-    def ready(self):
-        import api.signals
